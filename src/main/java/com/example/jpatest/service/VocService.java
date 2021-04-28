d920635ded445c072e0c95a0c93e8598711ec795
김창대 <cdkim1226@gmail.com>
2021-04-29 06:06:17 +0900

    Member Service Apply

---
+++ src/main/java/com/example/jpatest/service/VocService.java
@@ -0,0 +1,38 @@
+package com.example.jpatest.service;
+
+import com.example.jpatest.domain.Voc;
+import com.example.jpatest.repository.VocRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class VocService {
+
+    private final VocRepository vocRepository;
+
+    // @RequiredArgsConstructor 가 아래 내용을 추가 해줌
+//    @Autowired
+//    public VocService(VocRepository vocRepository) {
+//        this.vocRepository = vocRepository;
+//    }
+
+    public Long insert(Voc voc) {
+        vocRepository.save(voc);
+        return voc.getId();
+    }
+
+    public List<Voc> findVocList() {
+        return vocRepository.findAll();
+    }
+
+    public Voc findOne(Long vocSeq) {
+        return vocRepository.findOne(vocSeq);
+    }
+}
